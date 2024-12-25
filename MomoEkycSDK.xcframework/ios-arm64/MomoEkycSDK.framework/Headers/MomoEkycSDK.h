be71710a269ad722f43c8e529be75b4bb5a46be3
minh.nguyen3 <minh.nguyen3@mservice.com.vn>
2024-12-25 11:36:44 +0700

    Initial commit

---
+++ MomoEkycSDK.xcframework/ios-arm64/MomoEkycSDK.framework/Headers/MomoEkycSDK.h
@@ -0,0 +1,17 @@
+//
+//  MomoEkycSDK.h
+//  MomoEkycSDK
+//
+//  Created by Minh Nguyen on 4/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MomoEkycSDK.
+FOUNDATION_EXPORT double MomoEkycSDKVersionNumber;
+
+//! Project version string for MomoEkycSDK.
+FOUNDATION_EXPORT const unsigned char MomoEkycSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MomoEkycSDK/PublicHeader.h>
+#import <MomoEkycSDK/EkycCameraCoreCpp.h>
---
+++ MomoEkycSDK.xcframework/ios-x86_64-simulator/MomoEkycSDK.framework/Headers/EkycCameraCoreCpp.h
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EkycCameraCoreCpp : NSObject
+
+- (instancetype)init;
+- (NSString*)getKey: (NSString*)data;
+@end
+
+NS_ASSUME_NONNULL_END
